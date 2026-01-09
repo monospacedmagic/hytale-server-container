c0157fa2927cbabc29b32d50f12078a65ed4b501
deinfreu <49069598+deinfreu@users.noreply.github.com>
2026-01-09 01:51:35 +0100

    fix: fixed a path reference in the security.sh file

--- scripts/checks/security.sh
+++ scripts/checks/security.sh
@@ -3,7 +3,7 @@ set -eu
 
 # Load dependencies
 . "$SCRIPTS_PATH/utils.sh"
-. "$SCRIPTS_PATH/checks/lib/security.sh"
+. "$SCRIPTS_PATH/checks/lib/security_logic.sh"
 
 # Execute
 log "Starting security audit..." "$BLUE"
